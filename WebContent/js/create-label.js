73121487c3e2604779e84db68548b5ac1f92fd2f
anantkshirsagar <40562120+anantkshirsagar@users.noreply.github.com>
2019-12-24 21:10:31 +0530

    Filter create & grid info display done.

--- WebContent/js/create-label.js
+++ WebContent/js/create-label.js
@@ -7,7 +7,10 @@ function onPageLoad() {
 	 getLabelGridInfoService(null);
 }
 
+//Global variable which are used across the multiple functions
 var parentLabelsArray = [];
+var header = new Array();
+header = ['Sr.No', 'Label Name', 'Creation Date', 'Edit', 'Remove'];
 
 function getLabelGridInfoService(searchFilter){
 	var xhttp = new XMLHttpRequest();
@@ -16,7 +19,7 @@ function getLabelGridInfoService(searchFilter){
 		if (this.readyState == 4 && this.status == 200) {
 			var labelGridInfo = JSON.parse(xhttp.responseText);
 			console.log(labelGridInfo);
-			printTableData(labelGridInfo);
+			createTable(labelGridInfo);
 		}
 	};
 	
@@ -29,50 +32,145 @@ function getLabelGridInfoService(searchFilter){
 	xhttp.send();
 }
 
-function printTableData(gridInfo){
-	if(gridInfo){
-		var table = ""+
-		"<thead>"+
-			"<tr>"+
-				"<th>Sr No.</th>"+
-				"<th>Label Name</th>"+
-				"<th>Creation Date</th>"+
-				"<th>Action</th>"+
-			"</tr>"+
-		"</thead>";
-		for(var i = 0; i < gridInfo.length; ++i){
-			table += "<tbody>"+
-				"<tr>"+
-					"<td>"+gridInfo[i].srNo+"</td>"+
-					"<td>"+gridInfo[i].labelName+"</td>"+
-					"<td>"+gridInfo[i].creationDate+"</td>"+
-					"<td>"+
-						"<button type=\"button\" class=\"btn btn-default btn-danger\" onclick="+editLabelService(gridInfo[i])+" aria-label=\"Left Align\">"+
-							"Edit"+
-						"</button>&nbsp;&nbsp;"+
-						"<button type=\"button\" class=\"btn btn-default btn-danger\" aria-label=\"Left Align\">"+
-							"Delete"+
-						"</button>"+
-					"</td>"+
-				"</tr>"+
-			"</tbody>";
-		}
-		document.getElementById("printTable").innerHTML = table;
+function createTable(gridInfo) {
+	var table = document.createElement('table');
+	table.setAttribute('id', 'table'); // SET THE TABLE ID.
+	table.setAttribute('class','table table-striped valign');
+
+	var tr = table.insertRow(-1);
+
+	for (var i = 0; i < header.length; i++) {
+		var th = document.createElement('th'); // TABLE HEADER.
+		th.innerHTML = header[i];
+		tr.appendChild(th);
+	}
+
+	
+	var div = document.getElementById('printTableDiv');
+	div.innerHTML = "";
+	div.appendChild(table); // ADD THE TABLE TO YOUR WEB PAGE.
+	
+	addRow(gridInfo);
+}
+
+function addRow(gridInfo) {
+	var table = document.getElementById('table');
+
+	var rowCount = table.rows.length; // GET TABLE ROW COUNT.
+
+	for(var record = 0; record < gridInfo.length; ++record){
+		//Create new row each time
+		var tr = table.insertRow(record+1); // TABLE ROW.
+		tr = table.insertRow(record+1);
+
+		var td = document.createElement('td');
+		td = tr.insertCell(0);
+		var srNoCell = document.createTextNode(record+1);
+		td.appendChild(srNoCell);
+		
+		var labelNameTD = document.createElement('td');
+		labelNameTD = tr.insertCell(1);
+		var labelNameCell = document.createTextNode(gridInfo[record].labelName);
+		labelNameTD.appendChild(labelNameCell);
+		
+		var creationDateTD = document.createElement('td');
+		creationDateTD = tr.insertCell(2);
+		var creationDateCell = document.createTextNode(gridInfo[record].creationDate);
+		creationDateTD.appendChild(creationDateCell);
+		
+		var editButtonTD = document.createElement('td');
+		editButtonTD = tr.insertCell(3);
+		var name = gridInfo[record].id + "$" +gridInfo[record].labelName + "$" +gridInfo[record].creationDate;
+		var editButton = document.createElement('input');
+		editButton.setAttribute('type', 'button');
+		editButton.setAttribute('value', 'Edit');
+		editButton.setAttribute('name', name);
+		editButton.setAttribute('onclick', 'edit(this.name)');
+		editButton.setAttribute('class', 'btn btn-default btn-danger');
+		editButtonTD.appendChild(editButton);
+		
+		var deleteButtonTD = document.createElement('td');
+		deleteButtonTD = tr.insertCell(4);
+		var deleteButton = document.createElement('input');
+		deleteButton.setAttribute('type', 'button');
+		deleteButton.setAttribute('value', 'Delete');
+		deleteButton.setAttribute('onclick', 'edit(this)');
+		deleteButton.setAttribute('class', 'btn btn-default btn-danger');
+		deleteButtonTD.appendChild(deleteButton);	
 	}
 }
 
-function editLabelService(labelInfo){
-	console.log(labelInfo);
+var labelJson = {};
+var gmailLabelId = "";
+
+function edit(selectedLabelData){
+	labelJson = getLabelJson(selectedLabelData);
+	gmailLabelId = getGmailLabelId(labelJson.labelName);
+	var finalLabelName = getLabelNameFromLabelJson(labelJson.labelName);
+	
+	document.getElementById("oldLabelName").value = finalLabelName;
+	document.getElementById("editLabelName").value = finalLabelName;
+	openDialog("editModal");
+}
+
+function getLabelNameFromLabelJson(labelName){
+	if(labelName.includes('/')){
+		return labelName.split("/")[labelName.split("/").length-1];
+	}
+	return labelName;
+}
+
+function editLabelService(){
+	labelJson.labelName = document.getElementById("editLabelName").value;
+	labelJson.oldLabelName = document.getElementById("oldLabelName").value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			alert(JSON.parse(xhttp.responseText));
+			closeDialog("editModal");
+			getLabelGridInfoService(null);
+			getAllLabel();
+		}
+	};
+	
+	xhttp.open("POST", "CreateLabelServlet", true);
+	xhttp.setRequestHeader("gmailLabelId", gmailLabelId);
+	xhttp.setRequestHeader("userEmailId", sessionStorage.getItem("email"));
+	xhttp.setRequestHeader("callType", "EDIT_LABEL_SERVICE");
+	xhttp.send(JSON.stringify(labelJson));
+}
+
+
+function getGmailLabelId(labelName){
+	if(labelName){
+		for(var i = 0; i < parentLabelsArray.length; ++i){
+			if(parentLabelsArray[i].name == labelName){
+				return parentLabelsArray[i].id;
+			}
+		}
+	}
+}
+
+function getLabelJson(selectLabelData){
+	var delimeter = "$";
+	var split = selectLabelData.split(delimeter);
+	return {
+		id : split[0],
+		labelName : split[1],
+		oldLabelName : '',
+		creationDate : new Date(split[2])
+	};
 }
 
 function getAllLabel(){
+	parentLabelsArray = [];
 	var xhttp = new XMLHttpRequest();
-	var parentLabels = [];
 	
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			parentLabelsArray = JSON.parse(xhttp.responseText);
 			var selectTag = document.getElementById("parentLabelId");
+			clearAllOptions(selectTag);
 			var optionTag = document.createElement("option");
 			optionTag.text = "";
 			selectTag.options.add(optionTag, 0);
@@ -90,6 +188,12 @@ function getAllLabel(){
 	xhttp.send();
 }
 
+function clearAllOptions(selectTag){
+	if(selectTag) {
+		selectTag.options.length = 0;
+	}
+}
+
 function createLabelService() {
 	var xhttp = new XMLHttpRequest();
 	
@@ -105,9 +209,10 @@ function createLabelService() {
 	
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			console.log(JSON.parse(xhttp.responseText));
+			alert(xhttp.responseText);
 			getLabelGridInfoService(null);
-			closeDialog();
+			closeDialog("createModal");
+			getAllLabel();
 		}
 	};
 	xhttp.open("POST", "CreateLabelServlet", true);
@@ -121,6 +226,15 @@ function clearInputs(){
 	document.getElementById("labelName").value = "";
 }
 
-function closeDialog(){
-	$('#myModal').modal('hide');
+function closeDialog(dialogName){
+	$('#'+dialogName).modal('hide');
+}
+
+function openDialog(dialogName){
+	$('#'+dialogName).modal('show');
+}
+
+function reload(){
+	window.location = window.location;
+	getLabelGridInfoService(null);
 }
--- WebContent/js/label-filter.js
+++ WebContent/js/label-filter.js
@@ -3,6 +3,8 @@ function onPageLoad() {
 		window.location = "index.html";
 	}
 	document.getElementById("labelFilterSideNav").setAttribute("class","active");
+	getAllLabel();
+	getLabelFilterGridInfoService(null);
 }
 
 function getValue(id){
@@ -13,20 +15,59 @@ function getCheckboxValue(id){
 	return document.getElementById(id).checked;
 }
 
+var labelsArray = [];
+var header = new Array();
+header = ['Sr.No', 'Filter Name', 'Label Name', 'Creation Date', 'Edit', 'Remove', 'Run'];
+
+
+function clearAllOptions(selectTag){
+	if(selectTag) {
+		selectTag.options.length = 0;
+	}
+}
+
+function getAllLabel(){
+	labelsArray = [];
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			labelsArray = JSON.parse(xhttp.responseText);
+			var selectTag = document.getElementById("labelName");
+			clearAllOptions(selectTag);
+			var optionTag = document.createElement("option");
+			optionTag.text = "";
+			selectTag.options.add(optionTag, 0);
+			for(var i = 0; i < labelsArray.length; ++i){
+				optionTag = document.createElement("option");
+				optionTag.text = labelsArray[i].name;
+				selectTag.options.add(optionTag, i+1);
+			}			
+		}
+	};
+	
+	xhttp.open("POST", "CreateLabelServlet", true);
+	xhttp.setRequestHeader("userEmailId", sessionStorage.getItem("email"));
+	xhttp.setRequestHeader("callType", "GET_ALL_LABEL");
+	xhttp.send();
+}
+
 function labelFilterService() {
 	var xhttp = new XMLHttpRequest();
 	
 	var label = getValue("labelName");
 	var isEmailFilter = getCheckboxValue("isEmailFilter");
-	var emailIds = getValue("emailIds");
+	var filterName = getValue("filterName");
+	var emailIds = splitIntoArray(getValue("emailIds"));
 	var isSubjectFilter = getCheckboxValue("isSubjectFilter");
-	var subjectKeywords = getValue("subjectKeywords");
+	var subjectKeywords = splitIntoArray(getValue("subjectKeywords"));
 	var isBodyFilter = getCheckboxValue("isBodyFilter");
-	var bodyKeywords = getValue("bodyKeywords");
+	var bodyKeywords = splitIntoArray(getValue("bodyKeywords"));
 	
 	var deleteFilterJson = {
 		label : label,
 		isEmailFilter : isEmailFilter,
+		filterName : filterName,
 		emailIds : emailIds,
 		isSubjectFilter : isSubjectFilter,
 		subjectKeywords : subjectKeywords,
@@ -36,10 +77,126 @@ function labelFilterService() {
 	
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			console.log(JSON.parse(xhttp.responseText));
-			alert(xhttp.responseText);
+			getLabelFilterGridInfoService(null);
+			closeDialog("labelFilterModal");
 		}
 	};
 	xhttp.open("POST", "LabelFilterServlet", true);
+	xhttp.setRequestHeader("userEmailId", sessionStorage.getItem("email"));
+	xhttp.setRequestHeader("callType", "SAVE_LABEL_FILTER");
 	xhttp.send(JSON.stringify(deleteFilterJson));
+}
+
+function getLabelFilterGridInfoService(searchFilter){
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var labelGridInfo = JSON.parse(xhttp.responseText);
+			console.log(labelGridInfo);
+			createTable(labelGridInfo);
+		}
+	};
+	
+	xhttp.open("POST", "LabelFilterServlet", true);
+	if(searchFilter){
+		xhttp.setRequestHeader("searchFilter", searchFilter);
+	}
+	xhttp.setRequestHeader("userEmailId", sessionStorage.getItem("email"));
+	xhttp.setRequestHeader("callType", "LABEL_FILTER_GRID_INFO_SERVICE");
+	xhttp.send();
+}
+
+function createTable(gridInfo) {
+	var table = document.createElement('table');
+	table.setAttribute('id', 'table'); // SET THE TABLE ID.
+	table.setAttribute('class','table table-striped valign');
+
+	var tr = table.insertRow(-1);
+
+	for (var i = 0; i < header.length; i++) {
+		var th = document.createElement('th'); // TABLE HEADER.
+		th.innerHTML = header[i];
+		tr.appendChild(th);
+	}
+
+	
+	var div = document.getElementById('printTableDiv');
+	div.innerHTML = "";
+	div.appendChild(table); // ADD THE TABLE TO YOUR WEB PAGE.
+	
+	addRow(gridInfo);
+}
+
+
+function addRow(gridInfo) {
+	var table = document.getElementById('table');
+
+	var rowCount = table.rows.length; // GET TABLE ROW COUNT.
+
+	for(var record = 0; record < gridInfo.length; ++record){
+		//Create new row each time
+		var tr = table.insertRow(record+1); // TABLE ROW.
+		tr = table.insertRow(record+1);
+
+		var td = document.createElement('td');
+		td = tr.insertCell(0);
+		var srNoCell = document.createTextNode(record+1);
+		td.appendChild(srNoCell);
+		
+		var filterNameTD = document.createElement('td');
+		filterNameTD = tr.insertCell(1);
+		var filterNameCell = document.createTextNode(gridInfo[record].filterName);
+		filterNameTD.appendChild(filterNameCell);
+		
+		var labelNameTD = document.createElement('td');
+		labelNameTD = tr.insertCell(2);
+		var labelNameCell = document.createTextNode(gridInfo[record].label);
+		labelNameTD.appendChild(labelNameCell);
+		
+		var creationDateTD = document.createElement('td');
+		creationDateTD = tr.insertCell(3);
+		var creationDateCell = document.createTextNode(gridInfo[record].creationDate);
+		creationDateTD.appendChild(creationDateCell);
+		
+		var editButtonTD = document.createElement('td');
+		editButtonTD = tr.insertCell(4);
+		var name = gridInfo[record].id + "$" +gridInfo[record].labelName + "$" +gridInfo[record].creationDate;
+		var editButton = document.createElement('input');
+		editButton.setAttribute('type', 'button');
+		editButton.setAttribute('value', 'Edit');
+		editButton.setAttribute('name', name);
+		editButton.setAttribute('onclick', 'edit(this.name)');
+		editButton.setAttribute('class', 'btn btn-default btn-danger');
+		editButtonTD.appendChild(editButton);
+		
+		var deleteButtonTD = document.createElement('td');
+		deleteButtonTD = tr.insertCell(5);
+		var deleteButton = document.createElement('input');
+		deleteButton.setAttribute('type', 'button');
+		deleteButton.setAttribute('value', 'Delete');
+		deleteButton.setAttribute('onclick', 'edit(this)');
+		deleteButton.setAttribute('class', 'btn btn-default btn-danger');
+		deleteButtonTD.appendChild(deleteButton);	
+		
+		var runButtonTD = document.createElement('td');
+		runButtonTD = tr.insertCell(6);
+		var runButton = document.createElement('input');
+		runButton.setAttribute('type', 'button');
+		runButton.setAttribute('value', 'Run');
+		runButton.setAttribute('onclick', 'edit(this)');
+		runButton.setAttribute('class', 'btn btn-default btn-danger');
+		runButtonTD.appendChild(runButton);	
+	}
+}
+
+function splitIntoArray(string){
+	if(string){
+		return string.split(",");
+	}
+	return null;
+}
+
+function closeDialog(dialogName){
+	$('#'+dialogName).modal('hide');
 }
